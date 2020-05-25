7e1fb9aebfe0de860998028692a1a1ba549befbb
gabriel yago <gabriellok1212@gmail.com>
2020-05-24 21:16:50 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -6,17 +6,19 @@
 import express from 'express';
 import path from 'path';
 import routes from './routes';
+import cors from "cors";
 
 import './database';
 
 class App {
   constructor() {
     this.server = express();
-    this.middlewares();
+    this.middlewares()
     this.routes();
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
     /**
      * It serves to direct static files such as images, css and html files that
